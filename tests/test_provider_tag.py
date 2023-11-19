ffe51c1ab40449821ccd11834f3d4e520ce50751
Adaías Magdiel <adaiasmagdiell@gmail.com>
2023-11-19 20:01:15 -0300

    feat: add tags and tags_and_slug method test

--- tests/test_provider_tag.py
+++ tests/test_provider_tag.py
@@ -14,12 +14,28 @@ def test_provider_tag_is_a_string():
     assert isinstance(fake.article_tag(), str)
 
 
+def test_provider_has_tags_method():
+    assert hasattr(fake, "article_tags")
+
+
+def test_provider_tags_is_a_list():
+    assert isinstance(fake.article_tags(), list)
+
+
 def test_provider_has_tag_and_slug_method():
     assert hasattr(fake, "article_tag_and_slug")
 
 
 def test_provider_tag_and_slug_is_a_dict():
     assert isinstance(fake.article_tag_and_slug(), dict)
+
+
+def test_provider_has_tags_and_slug_method():
+    assert hasattr(fake, "article_tags_and_slug")
+
+
+def test_provider_tags_and_slug_is_a_list():
+    assert isinstance(fake.article_tags_and_slug(), list)
 
 
 def test_slugify_can_slugify_tag():
